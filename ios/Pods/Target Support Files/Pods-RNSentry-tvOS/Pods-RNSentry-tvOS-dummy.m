c19e3357ef4ccebfef30f5da2cc0c998c946e53b
gispatial <react7tech@gmail.com>
2021-01-14 09:34:16 +0800

    pushy-update

---
+++ ios/Pods/Target Support Files/Pods-RNSentry-tvOS/Pods-RNSentry-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNSentry_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_RNSentry_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-RNSentry/Pods-RNSentry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNSentry : NSObject
+@end
+@implementation PodsDummy_Pods_RNSentry
+@end
